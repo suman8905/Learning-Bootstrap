a126ce38205d8ccab855e8c3bf8b381adc1a3e16
Sushmita <sushmitasuman0105@gmail.com>
2024-05-21 12:47:56 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <div>
+ return <div>
    <button type="button" class="btn btn-primary">Primary</button>
 <button type="button" class="btn btn-secondary">Secondary</button>
 <button type="button" class="btn btn-success">Success</button>
